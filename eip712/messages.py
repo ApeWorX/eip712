3c3052afbbcce4181fc1b8bfc20d4720b674d57c
Steven Kryskalla <skryskalla@gmail.com>
2021-05-26 11:35:04 -0400

    feature: add property to EIP712Message that returns a SignableMessage
    The `SignableMessage` can then be passed to `_hash_eip191_message`,
    which I've also added from eth_account. This will make it easier for
    end users to hash and sign `EIP712Message`s.

--- eip712/messages.py
+++ eip712/messages.py
@@ -1,15 +1,15 @@
-from typing import Dict
+from typing import Dict, NamedTuple
 
 from dataclassy import as_dict, dataclass, fields
 from eth_abi import is_encodable_type
-from eth_utils.curried import ValidationError
+from eth_typing import Hash32
+from eth_utils.curried import ValidationError, keccak
+from hexbytes import HexBytes
 
 # isort: off
 # We need isort off for these two imports because black makes a conflicting change.
 from .hashing import hash_domain  # type:ignore
-from .hashing import (
-    hash_message as hash_eip712_message,
-)
+from .hashing import hash_message as hash_eip712_message
 
 EIP712_DOMAIN_FIELDS = [
     "name",
@@ -18,6 +18,41 @@ EIP712_DOMAIN_FIELDS = [
     "verifyingContract",
 ]
 HEADER_FIELDS = set(f"_{field}_" for field in EIP712_DOMAIN_FIELDS)
+
+
+# https://github.com/ethereum/eth-account/blob/f1d38e0/eth_account/messages.py#L39
+class SignableMessage(NamedTuple):
+    """
+    These are the components of an EIP-191_ signable message. Other message formats
+    can be encoded into this format for easy signing. This data structure doesn't need to
+    know about the original message format.
+
+    In typical usage, you should never need to create these by hand. Instead, use
+    one of the available encode_* methods in this module, like:
+
+        - :meth:`encode_structured_data`
+        - :meth:`encode_intended_validator`
+        - :meth:`encode_structured_data`
+
+    .. _EIP-191: https://eips.ethereum.org/EIPS/eip-191
+    """
+
+    version: bytes  # must be length 1
+    header: bytes  # aka "version specific data"
+    body: bytes  # aka "data to sign"
+
+
+# https://github.com/ethereum/eth-account/blob/f1d38e0/eth_account/messages.py#L59
+def _hash_eip191_message(signable_message: SignableMessage) -> Hash32:
+    version = signable_message.version
+    if len(version) != 1:
+        raise ValidationError(
+            "The supplied message version is {version!r}. "
+            "The EIP-191 signable message standard only supports one-byte versions."
+        )
+
+    joined = b"\x19" + version + signable_message.header + signable_message.body
+    return Hash32(keccak(joined))
 
 
 @dataclass(iter=True, slots=True)
@@ -115,3 +150,11 @@ class EIP712Message(EIP712Type):
     @property
     def body(self) -> bytes:
         return hash_eip712_message(self.body_data)
+
+    @property
+    def signable_message(self) -> SignableMessage:
+        return SignableMessage(
+            HexBytes(b"\x01"),
+            self.header,
+            self.body,
+        )
