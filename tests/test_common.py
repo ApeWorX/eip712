0b9fd70a03fc72c55e3af32e694f12308a943748
Daniel Schiavini <daniel.schiavini@gmail.com>
2024-05-21 20:57:09 +0200

    Update tests

--- tests/test_common.py
+++ tests/test_common.py
@@ -1,5 +1,5 @@
 import pytest
-from eth_account._utils.structured_data.hashing import hash_message
+from eth_account.messages import encode_typed_data, _hash_eip191_message
 from hexbytes import HexBytes
 
 from eip712.common import SAFE_VERSIONS, create_safe_tx_def
@@ -20,12 +20,12 @@ def test_gnosis_safe_tx(version):
     msg = tx_def(to=MAINNET_MSIG_ADDRESS, nonce=0)
 
     assert msg.signable_message.header.hex() == (
-        "0x88fbc465dedd7fe71b7baef26a1f46cdaadd50b95c77cbe88569195a9fe589ab"
+        "88fbc465dedd7fe71b7baef26a1f46cdaadd50b95c77cbe88569195a9fe589ab"
         if version in ("1.3.0",)
-        else "0x590e9c66b22ee4584cd655fda57748ce186b85f829a092c28209478efbe86a92"
+        else "590e9c66b22ee4584cd655fda57748ce186b85f829a092c28209478efbe86a92"
     )
 
-    assert hash_message(msg).hex() == (
+    assert msg.signable_message.body.hex() == (
         "3c2fdf2ea8af328a67825162e7686000787c5cc9f4b27cb6bfbcaa445b59e2c4"
         if version in ("1.3.0",)
         else "1b393826bed1f2297ffc01916f8339892f9a51dc7f35f477b9a5cdd651d28603"
