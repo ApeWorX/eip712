76bbf14fb573c3c888223ce9223a086dacd9dea0
fubuloubu <3859395+fubuloubu@users.noreply.github.com>
2025-12-11 18:57:02 -0500

    feat: export `EIP712Domain` class as well

--- eip712/__init__.py
+++ eip712/__init__.py
@@ -1,7 +1,8 @@
-from .messages import EIP712Message, EIP712Type, hash_message
+from .messages import EIP712Domain, EIP712Message, EIP712Type, hash_message
 
 __all__ = [
+    "EIP712Domain",
     "EIP712Message",
-    "EIP712Type",
+    "EIP712Type",  # NOTE: Only for backwards compatibility (remove in v1)
     "hash_message",
 ]
